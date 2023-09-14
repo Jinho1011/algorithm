66042e63eb8239ad8a0ea6634e172081998b54fd
kang yeon joo <rkdduswndi@naver.com>
2023-09-14 15:29:27 +0900

    feat: solve 18312

--- yeonjookang/1주차/18312.시각.py
+++ yeonjookang/1주차/18312.시각.py
@@ -11,6 +11,8 @@ for i in range(0,60):
     time_초.append(i)
 
 for i in range(0,N+1):
+    if i<10: str_i='0'+str(i)
+    else: str_i=str(i)
     for 분 in time_분:
         for 초 in time_초:
             if 분<10: str_분='0'+str(분)
@@ -18,6 +20,6 @@ for i in range(0,N+1):
             if 초<10: str_초='0'+str(초)
             else: str_초=str(초)
 
-            if str(i).count(str(K))>0 or str_분.count(str(K))>0 or str_초.count(str(K))>0 :
+            if str_i.count(str(K))>0 or str_분.count(str(K))>0 or str_초.count(str(K))>0 :
                 answer+=1
 print(answer)
