9d9b9ef49e17dfe07cb317267d08f803dee10302
정소민 <yjk70702002@gmail.com>
2024-01-20 20:25:36 +0900

    feat: solve 10828

---
+++ somin-jeong/알고리즘 기초 1-1/자료구조/10828 스택.py
@@ -0,0 +1,28 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+stack = []
+size = 0
+
+for i in range(N):
+    input_str=sys.stdin.readline()
+    if input_str.startswith("push"):
+        stack.append(input_str.split()[1])
+    if input_str.startswith("pop"):
+        if len(stack)!=0:
+            print(stack.pop())
+        else:
+            print(-1)
+    if input_str.startswith("size"):
+        print(len(stack))
+    if input_str.startswith("empty"):
+        if(len(stack)==0):
+            print(1)
+        else:
+            print(0)
+    if input_str.startswith("top"):
+        if len(stack)!=0:
+            print(stack[len(stack)-1])
+        else:
+            print(-1)
